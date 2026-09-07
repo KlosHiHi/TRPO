481792964dd05156831129d72e7000868f58bbb7
DeDomosed <poperm137@gmail.com>
2026-09-07 12:52:19 +0300

    lr1

---
+++ labs/ЛР1/DUO52/DUO52/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace DUO52
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Authorization_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+            Registration reg = new();
+            reg.ShowDialog();
+        }
+
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new()
+            {
+                Filter = "(*.csv)|*.csv|(*.txt)|*.txt",
+                Title = "Выберите файл для импорта"
+            };
+
+            if (!dialog.ShowDialog() == true)
+            {
+                MessageBox.Show("Ошибка");
+                return;
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new()
+            {
+                Filter = "(*.csv)|*.csv|(*.txt)|*.txt",
+                Title = "Выберите путь для сохранения"
+            };
+
+            if (!dialog.ShowDialog() == true)
+            {
+                MessageBox.Show("Ошибка");
+                return;
+            }
+        }
+    }
+}
---
+++ labs/ЛР1/DUO52/DUO52/Registration.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DUO52
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginTextBox.Text;
+            var pass = PasswordTextBox.Text;
+            File.WriteAllText("pass.txt", $"{login} - {pass}");
+        }
+    }
+}
