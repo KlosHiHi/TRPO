e6ecbb08ad4b3854317978d17a929f2a1f4a9224
DeDomosed <poperm137@gmail.com>
2026-09-10 10:06:33 +0300

    pravki

---
+++ labs/ЛР2/LabWork2API/LabWork2API/TaskStatus.cs
@@ -0,0 +1,15 @@
+namespace LabWork2API
+{
+    public class TaskStatus
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public short Priority { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
