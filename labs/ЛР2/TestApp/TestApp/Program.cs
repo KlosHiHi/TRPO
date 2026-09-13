34216de8b38f34311330df65b102b9e3182b6e4e
KlosHiHi <vaceslavvlasov420@gmail.com>
2026-09-13 20:27:02 +0300

    lab2

--- labs/ЛР2/TestApp/TestApp/Program.cs
+++ labs/ЛР2/TestApp/TestApp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TestApp;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Task1.SafePassword("21ferGG$"));
+
+Console.WriteLine(Task1.SafePassword("ferGGsad25"));
+Console.WriteLine(Task1.SafePassword("ferGG$$sad"));
+Console.WriteLine(Task1.SafePassword("ffG#2"));
+Console.WriteLine(Task1.SafePassword("15ger23ger31ger43ger$54ger6678$RR"));
+Console.WriteLine(Task1.SafePassword("33ffGПар12"));
+Console.WriteLine(Task1.SafePassword("GER12ASP"));
+Console.WriteLine(Task1.SafePassword("goal333%12"));
